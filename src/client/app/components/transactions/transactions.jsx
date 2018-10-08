a81fe0edf63b6e9ef724d6a357e7182fbcbe8334
Gabor Vajda <vajdagabor@users.noreply.github.com>
2018-10-08 16:07:17 +0200

    Format date and amount in the transaction tables

--- src/client/app/components/transactions/transactions.jsx
+++ src/client/app/components/transactions/transactions.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import "./transactions.scss";
 
+const numberFormatter = Intl.NumberFormat("en-GB", {
+  minimumFractionDigits: 2
+});
+
+const enDate = date =>
+  new Date(date).toLocaleDateString("en-GB", {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  });
+
+const money = amount => numberFormatter.format(amount.toFixed(2));
+
 const Transactions = props => (
   <table className="c-Transactions">
     <tbody>
-      {props.data.map((trans, index) =>
+      {props.data.map((trans, index) => (
         <tr key={index} className="c-Transactions__Row">
-          <td className="c-Transactions__Date">{trans.date}</td>
-          <td className="c-Transactions__Amount">£{trans.amount}</td>
+          <td className="c-Transactions__Date">{enDate(trans.date)}</td>
+          <td className="c-Transactions__Amount">
+            <span className="c-Transactions__Currency">£</span>{money(trans.amount)}
+          </td>
         </tr>
-      )}
+      ))}
     </tbody>
   </table>
 );
