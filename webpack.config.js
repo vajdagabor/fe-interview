444c99b0224f9b30056749fcce47c706de0abdc8
Gabor Vajda <vajdagabor@users.noreply.github.com>
2018-10-08 11:33:38 +0200

    Set up the environment

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
+var COMPONENT_DIR = path.resolve(__dirname, 'src/client/app/components');
+var STYLESHEET_DIR = path.resolve(__dirname, 'src/client/app/stylesheets');
 
 var config = {
   entry: APP_DIR + '/index.jsx',
@@ -11,13 +13,26 @@ var config = {
     filename: 'bundle.js'
   },
   module : {
-    loaders : [
+    rules: [
       {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel-loader'
-      }
-    ]
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"],
+        include: [
+          COMPONENT_DIR,
+          STYLESHEET_DIR
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
   }
 };
 
